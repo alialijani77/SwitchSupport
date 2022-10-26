8cf1b125a90f9c78c3f737b6f2098220021ec972
ali <alialijani1377@gmail.com>
2022-10-26 12:52:40 +0330

    QuestionController

---
+++ SwitchSupport.Web/Controllers/QuestionController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SwitchSupport.Web.Controllers
+{
+    public class QuestionController : BaseController
+    {
+        #region Question
+        [Authorize]
+        [HttpGet("create-question")]
+        public IActionResult CreateQuestion()
+        {
+            return View();
+        }       
+        #endregion
+    }
+}
