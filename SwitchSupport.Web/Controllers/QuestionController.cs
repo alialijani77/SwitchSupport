b68ecd0c37e6724da23face0f9be55244406eacb
ali <alialijani1377@gmail.com>
2022-10-31 21:49:00 +0330

    Add Magicsuggest Tags

--- SwitchSupport.Web/Controllers/QuestionController.cs
+++ SwitchSupport.Web/Controllers/QuestionController.cs
@@ -1,17 +1,40 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using SwitchSupport.Application.Services.Interfaces;
 
 namespace SwitchSupport.Web.Controllers
 {
     public class QuestionController : BaseController
     {
+        private readonly IQuestionService _questionService;
+
+        #region ctor
+        public QuestionController(IQuestionService questionService)
+        {
+            _questionService = questionService;
+        }
+        #endregion
         #region Question
         [Authorize]
         [HttpGet("create-question")]
         public IActionResult CreateQuestion()
         {
             return View();
-        }       
+        }
         #endregion
+
+        #region Tags
+        [HttpGet("get-tags")]
+        public async Task<IActionResult> GetTags(string name)
+        {
+            var tags = await _questionService.GetAllTags();
+            var qu = tags.Where(t => t.Title.Contains(name)).Select(t=>t.Title).ToList();
+
+            return Json(qu);
+        }
+
+        #endregion
+
+
     }
 }
