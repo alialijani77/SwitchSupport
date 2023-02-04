99ab1f7150160d2182c45fe5ce2713fad9e20b7d
ali <alialijani1377@gmail.com>
2023-02-04 23:33:36 +0330

    LeftSide-NewQuestion

---
+++ SwitchSupport.Web/ViewComponents/NewQuestionViewComponent.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SwitchSupport.Application.Services.Interfaces;
+using SwitchSupport.Domain.ViewModels.Question;
+
+namespace SwitchSupport.Web.ViewComponents
+{
+    public class NewQuestionViewComponent : ViewComponent
+    {
+        private readonly IQuestionService _questionService;
+
+        public NewQuestionViewComponent(IQuestionService questionService)
+        {
+            _questionService = questionService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var options = new FilterQuestionViewModel
+            {
+                TakeEntitiy = 5,
+                Sort = FilterQuestionSortEnum.NewToOld
+            };
+
+            var result = await _questionService.GetAllQuestions(options);
+
+
+            return View("NewQuestion", result);
+        }
+
+    }
+}
