ceef932ebad0088294b1df11acc47585cd89e546
ali <alialijani1377@gmail.com>
2023-02-05 22:08:46 +0330

    LeftSide-UseCountHighToLowTags

---
+++ SwitchSupport.Web/ViewComponents/UseCountHighToLowTagsViewComponent.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SwitchSupport.Application.Services.Interfaces;
+using SwitchSupport.Domain.ViewModels.Question;
+
+namespace SwitchSupport.Web.ViewComponents
+{
+    public class UseCountHighToLowTagsViewComponent : ViewComponent
+    {
+        private readonly IQuestionService _questionRepository;
+
+        public UseCountHighToLowTagsViewComponent(IQuestionService questionRepository)
+        {
+            _questionRepository = questionRepository;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var options = new FilterTagViewModel
+            {
+                Sort = FilterTagSortEnum.UseCountHighToLow,
+                TakeEntitiy = 5
+            };
+
+            var result = await _questionRepository.GetAllFilterTags(options);
+
+            return View("UseCountHighToLowTags", result);
+        }
+
+    }
+}
