b034cdbba7deca18afb126d3c1421575021f1227
ali <alialijani1377@gmail.com>
2023-02-12 22:20:02 +0330

    AdminPanel

---
+++ SwitchSupport.Web/Areas/Admin/Controllers/AdminBaseController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SwitchSupport.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize]
+    public class AdminBaseController : Controller
+    {
+        public static readonly string SuccessMessage = "SuccessMessage";
+        public static readonly string InfoMessage = "InfoMessage";
+        public static readonly string WarningMessage = "WarningMessage";
+        public static readonly string ErrorMessage = "ErrorMessage";
+    }
+}
